32b38cabd516db462bab4f1b1031d74bfcf1b17c
Suman Naik Guguloth <suman.guguloth@vwo.com>
2024-10-09 16:15:35 +0530

    configure tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import React from "react";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div>Hi this is the app</div>
+    <div>
+      <Body />
+    </div>
   );
 }
 
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='text-4xl bg-green-300'>Body</div>
+  )
+}
+
+export default Body

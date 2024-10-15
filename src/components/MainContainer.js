34a1484e4510c2ca15801cc46dc4974843ee18ad
Suman Naik Guguloth <suman.guguloth@vwo.com>
2024-10-15 21:51:47 +0530

    change api key

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -12,9 +12,8 @@ const MainContainer = () => {
         <MovieDetails />
         <iframe
             className="w-screen md:h-screen h-full aspect-video"
-            src={"https://www.youtube.com/embed/" + trailerVideoDetails?.key + "?rel=0&showinfo=0&autoplay=1&mute=1&controls=0&modestbranding=1&vq=hd720"}
+            src={"https://www.youtube.com/embed/" + trailerVideoDetails?.key + "?rel=0&showinfo=0&autoplay=1&mute=1&controls=0&modestbranding=1&vq=hd720&playsinline=1"}
             title="YouTube video player"
-            framebBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             >
         </iframe>
